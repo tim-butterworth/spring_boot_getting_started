b364bf682fa01577ab87aec8991d649472a1e829
Michael R Fortenberry <Michael R Fortenberry>
2016-05-01 15:14:28 -0400

    remove issues line without parent and add issues 4 through 10 for Amazing Spider-Man just to show some changes I commited using git

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,9 +12,15 @@ INSERT INTO Volume (name) VALUES ('Volume 2')
 INSERT INTO Volume (name) VALUES ('Volume 3')
 
 INSERT INTO Issue (issue_number, title) VALUES (1, 'Spider-Man')
-INSERT INTO Issue (issue_number, title) VALUES (2, 'Enter the Vulture')
-INSERT INTO Issue (issue_number, title) VALUES (3, 'Doctor Octopus')
-
+INSERT INTO Issue (issue_number, title) VALUES (2, 'Duel to the Death With The Vulture')
+INSERT INTO Issue (issue_number, title) VALUES (3, 'Spider-Man Vs. Doctor Octopus')
+INSERT INTO Issue (issue_number, title) VALUES (4, 'Nothing Can Stop... The Sandman!')
+INSERT INTO Issue (issue_number, title) VALUES (5, 'Marked For Destruction by Dr. Doom!')
+INSERT INTO Issue (issue_number, title) VALUES (6, 'Face-To-Face With... The Lizard!')
+INSERT INTO Issue (issue_number, title) VALUES (7, 'The Return of The Vulture!')
+INSERT INTO Issue (issue_number, title) VALUES (8, 'The Terrible Threat of The Living Brain!')
+INSERT INTO Issue (issue_number, title) VALUES (9, 'The Man Called Electro!')
+INSERT INTO Issue (issue_number, title) VALUES (10, 'The Enforcers!')
 
 INSERT INTO publisher_title (publisher_id, title_id) VALUES ((select id from Publisher where name = 'Marvel'), (select id from Title where name = 'The Amazing Spider-Man'));
 INSERT INTO publisher_title (publisher_id, title_id) VALUES ((select id from Publisher where name = 'DC'), (select id from Title where name = 'Detective Comics'));
